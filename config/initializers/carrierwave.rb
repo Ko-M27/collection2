e9d3f333954519f2e85628d05f83bc32ef2b809c
Ko-M27 <m.kodai9527@gmail.com>
2020-09-10 21:38:56 +0900

    photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? 
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
-    }
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1'
+  }
 
-    config.fog_directory  = 'collection2-bucket'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/collection2-bucket'
-  else
-    config.storage = :file
-  end
+  config.fog_directory  = 'collection2-bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/collection2-bucket'
+  config.fog_public = false
 end
